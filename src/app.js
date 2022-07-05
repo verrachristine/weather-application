ff67bfda0ac6f40b759862e7a334acab2ce27808
verrachristine <verratuboro@gmail.com>
2022-07-04 23:02:17 -0400

    Showing the current temperature, description, humidity, and wind speed

--- src/app.js
+++ src/app.js
@@ -1,1 +1,22 @@
+function displayTemperature(response) {
+  let cityElement = document.querySelector("#current-city");
+  let countryElement = document.querySelector("#current-country");
+  let temperatureElement = document.querySelector("#current-temp");
+  let descriptionElement = document.querySelector("#current-description");
+  let humidityElement = document.querySelector("#humidity-content");
+  let windElement = document.querySelector("#wind-content");
+  cityElement.innerHTML = response.data.name;
+  countryElement.innerHTML = response.data.sys.country;
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity + "%";
+  windElement.innerHTML = Math.round(response.data.wind.speed) + " m/s";
 
+  console.log(response.data);
+}
+
+let apiKey = "f86609e405cc74813bc6bd5906b03dac";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Miami&appid=${apiKey}&units=metric`;
+
+console.log(apiUrl);
+axios.get(apiUrl).then(displayTemperature);
